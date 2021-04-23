36e20e52e6b9f598f21f8609b316d856404306e5
Marcelo Crístian <82716012+srcmarcelo@users.noreply.github.com>
2021-04-22 21:43:32 -0300

    So pra testar
    Profundo

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <raylib.h>
+#include <stdio.h>
+
+void Combat_LeaoNidas(int *vida_de_rubens){
+    //iniciar musica combat
+    //dialogo entre Leao Nidas e Rubens
+    int vida_de_leao = 70;
+}
+
+int main(){
+    int *vida_de_Rubens;
+    Combat_LeaoNidas(&vida_de_Rubens);
+}

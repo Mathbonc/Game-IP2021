58bdff922668058c7fe63cb8b0f77d3525ec1a8e
Marcelo Crístian <82716012+srcmarcelo@users.noreply.github.com>
2021-04-26 06:11:36 -0300

    Combates e a função rand()...
    Agora a escolha dos ataques dos alunos será aleatória. Além disso, coloquei as funções principais que serão utilizadas. Antes de preenche-las quero pensar em mais ataques diferentes(pelo menos para os alunos, talvez um especial para cada um), mas qualquer coisa preencho assim mesmo.

--- src/Combat.c
+++ src/Combat.c
@@ -1,5 +1,6 @@
 #include <raylib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct{
     int Questao_complicada, Correção_errada, Nota_Errada, Questão_Oral;
@@ -37,14 +38,14 @@ void Rubens_atack(Texture2D warrior, Vector2 position, int frame, float timer, f
 
 
 void Combat_LeaoNidas(int *vida_de_rubens){
-    int vida_de_leao = 70, opRubens, opAluno;
+    int vida_de_leao = 70, opRubens, opAluno, flag = 0;
     ataques_rubens ataqueRubens; ataques_aluno ataqueLeao;
     ataqueRubens.Questao_complicada = 10; ataqueRubens.Correção_errada = 30;
     ataqueRubens.Nota_Errada = 40; ataqueRubens.Questão_Oral = 70;
     //iniciar musica combat
     //dialogo entre Leao Nidas e Rubens
     while(vida_de_leao > 0 && vida_de_rubens > 0){
-        //menu de opcoes para Rubens de 1 a 2 no primeiro ataque
+        if(flag == 0) /*menu de opcoes para Rubens de 1 a 2*/;
         scanf("%d", &opRubens);
         switch (opRubens){
         case 1:
@@ -63,7 +64,10 @@ void Combat_LeaoNidas(int *vida_de_rubens){
             break;
         }
         //menu de opcoes para aluno
-        scanf("%d", &opAluno);
+        srand((unsigned)time(NULL));
+        do{
+            opAluno =  (rand() % 10);
+        }while(opAluno < 1 || opAluno > 2);
         switch (opAluno){
         case 1:
             vida_de_rubens -= ataqueLeao.acertar_questao;
@@ -77,7 +81,27 @@ void Combat_LeaoNidas(int *vida_de_rubens){
     }
 }
 
+void Combat_RoBrigo(int *vida_de_rubens){
+
+}
+
+void Combat_MuLittle(int *vida_de_rubens){
+
+}
+
+void Combat_XAnny(int *vida_de_rubens){
+
+}
+
+void Combat_Freddy(int *vida_de_rubens){
+
+}
+
 int main(){
-    int *vida_de_Rubens;
+    int vida_de_Rubens;
     Combat_LeaoNidas(&vida_de_Rubens);
+    Combat_RoBrigo(&vida_de_Rubens);
+    Combat_MuLittle(&vida_de_Rubens);
+    Combat_XAnny(&vida_de_Rubens);
+    Combat_Freddy(&vida_de_Rubens);
 }

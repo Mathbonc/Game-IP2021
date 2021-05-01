f170664b0b05d1e8632a9251d297144b19447b90
Paulinho Souza <82713803+xpauloszzx@users.noreply.github.com>
2021-05-01 16:41:49 -0300

    update lib

--- src/combate.h
+++ src/combate.h
@@ -10,11 +10,24 @@ typedef struct{ //Struct para os ataques do aluno
     int acertar_questao, discutir_questao, chamar_no_zap;
 } ataques_aluno;
 
+typedef struct {
+	Sound musica[10];
+}mus;
+
+typedef struct{
+    Texture2D Rubens[4];
+}player;
+
 void MenudeRubens(ataques_rubens ataques, int *vida_estudante, int vida_Rubens, int *opRubens);
 void Combat_LeaoNidas(int *vida_de_rubens);
 void Combat_RoBrigo(int *vida_de_rubens);
 void Combat_MuLittle(int *vida_de_rubens);
 void Combat_XAnny(int *vida_de_rubens);
 void Combat_Freddy(int *vida_de_rubens);
+player CarregaSpriteRubens(player rbs);
+mus CarregaMusicas(mus Song);
+void TrocaMusicaCombate(Sound musica_anterior, Sound musica_proxima);
+	
+
 
 #endif
